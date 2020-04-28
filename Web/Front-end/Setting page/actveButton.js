7558602a8b77346830cdf1588792503fd4f56da4
TakomY <63721877+TakomY@users.noreply.github.com>
2020-04-28 22:23:08 +0300

    Delete actveButton.js

--- Web/Front-end/Setting page/actveButton.js
+++
@@ -1,6 +0,0 @@
-$('#fucking_buttons a').click(function(e) {
-  e.preventDefault();
-  $("#fucking_buttons li").attr("id","");
-  $(this).parent().attr("id","current"); 
-  $('#' + $(this).attr('title')).fadeIn(); 
-  });
